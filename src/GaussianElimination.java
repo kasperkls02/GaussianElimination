4841115e4c2fccad57b225f0da23066479f6283e
Kasper Læssø Sørensen <kasperkls02@gmail.com>
2021-12-02 14:15:55 +0100

    Updated for my preferences

--- src/GaussianElimination.java
+++ src/GaussianElimination.java
@@ -5,17 +5,20 @@ import java.io.IOException;
 public class GaussianElimination {
 	public static void main(String[] args) throws IOException {
 		Object[][] matEntries;
-		int nrows,ncols,dash,t;
+		int nrows,ncols,dash,t,kasper;
 		Matrix matrix;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter number of rows: ");
 		nrows=input.nextInt();
 		System.out.println("Enter number of columns: ");
 		ncols=input.nextInt();
-		System.out.println("Enter position of vertical dash (-1 for none): ");
-		dash=input.nextInt();
-		System.out.println("Enter type of matrix (0 for Z_p, 1 for rational numbers): ");
-		t=input.nextInt();
+		System.out.println("Enter position of vertical dash (0 for none): ");
+		kasper=input.nextInt();
+		dash=ncols;
+		
+		//System.out.println("Enter type of matrix (0 for Z_p, 1 for rational numbers): ");
+		//t=input.nextInt();
+		t=1;
 		if(t==0) {
 			System.out.println("Enter p: ");
 			int p=input.nextInt();
@@ -26,7 +29,7 @@ public class GaussianElimination {
 					matEntries[i][j]=new IntModP(0);
 				}
 			}
-			matrix=new Matrix<IntModP>((IntModP[][])matEntries,dash);
+			matrix=new Matrix<IntModP>((IntModP[][])matEntries,dash,kasper);
 		}
 		else {
 			matEntries=new Rational[nrows][ncols];
@@ -35,7 +38,7 @@ public class GaussianElimination {
 					matEntries[i][j]=new Rational(0);
 				}
 			}
-			matrix=new Matrix<Rational>((Rational[][])matEntries,dash);
+			matrix=new Matrix<Rational>((Rational[][])matEntries,dash,kasper);
 		}
 		input.nextLine();
 		System.out.println("Enter matrix rows:");
@@ -44,6 +47,7 @@ public class GaussianElimination {
 			line=input.nextLine();
 			matrix.parseFill(line,i);
 		}
+		input.close();
 		FileWriter f=new FileWriter("out.tex");
 		String result=matrix.gaussElim();
 		//System.out.println(result);
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,29 +1,24 @@
-import java.util.Vector;
+//import java.util.Vector;
 
 public class Matrix<T extends Arithmetic<T> & Parse & Latex & HasDelim> {
 	T[][] mat;
 	int vdash;
+	int vkasper;
 	StringBuffer latexLog;
-	String preamble=  "\\documentclass{article}\n"
-					 +"\\usepackage{fancyhdr}\n"
-					 +"\\usepackage[utf8]{inputenc}\n"
-					 +"\\usepackage{amsmath}\n"
-					 +"\\usepackage{amssymb}\n"
-					 +"\\usepackage{graphicx}\n"
+	String preamble=  "\\documentclass[preview]{standalone}\n"
+					 +"\\newcommand{\\BAR}{%\n	\\hspace{-\\arraycolsep}%\n	\\strut\\vrule % the `\\vrule` is as high and deep as a strut\n	\\hspace{-\\arraycolsep}%\n}\n"
 					 +"\\usepackage{gauss}\n"
-					 +"\\pagestyle{fancy}\n"
-					 +"\\usepackage{etoolbox}\n"
-					 +"\\setlength{\\parskip}{\\baselineskip}\n"
-					 +"\\setlength{\\headheight}{24pt}\n"
-					 +"\\setlength\\parindent{0pt}\n"
-					 +"\\newmatrix{|}{)}{q}\n"
-					 +"\\begin{document}\n";
-	String post="\\end{document}";
+					 +"\\newmatrix{[}{]}{q}\n"
+					 +"\\begin{document}\n"
+					 +"\\begin{align*}";
+	String post="\\end{align*}\n"
+	+"\\end{document}";
 	String ldelim="\\left(";
 	String rdelim="\\right)";
-	public Matrix(T[][] matrix,int dash) {
+	public Matrix(T[][] matrix,int dash, int kasper) {
 		mat=matrix;
 		vdash=dash;
+		vkasper=kasper;
 	}
 	public Matrix(T[][] matrix) {
 		mat=matrix;
@@ -73,14 +68,19 @@ public class Matrix<T extends Arithmetic<T> & Parse & Latex & HasDelim> {
 		}
 	}
 	public String latex(String ops) {
+		//System.out.println();
 		int mid=(vdash==-1)?mat[0].length:vdash;
 		
-		StringBuffer result=new StringBuffer("$\\linespread{1.2}\\selectfont");
-		result.append((vdash==-1)?"\\begin{gmatrix}[p]\n":"\\left(\\begin{gmatrix}\n");
+		StringBuffer result=new StringBuffer("&\\linespread{1.2}\\selectfont");
+		result.append((vdash==-1)?"\\begin{gmatrix}[p]\n":"\\begin{gmatrix}[q]\n");
 		
 		for(int i=0;i<mat.length;++i) {
 			for(int j=0;j<mid;++j) {
+				if(j==vkasper && j!=0) {
+					result.append(" \\BAR & ");
+				}
 				result.append(mat[i][j].latex());
+				
 				if(j==(mid-1) && i<mat.length-1) {
 					result.append(" \\\\\n");
 				}
@@ -89,10 +89,11 @@ public class Matrix<T extends Arithmetic<T> & Parse & Latex & HasDelim> {
 				}
 			}
 		}
+		//result.append("\\end{gmatrix}\n");
 		
-		if(vdash!=-1) {
-			result.append("\\end{gmatrix}\\right.\n"
-					+ "\\begin{gmatrix}[q]\n");
+		/*if(vdash!=-1) {
+			result.append("\\end{matrix}\n"
+					+ "\\begin{gmatrix}\n");
 			for (int i = 0; i < mat.length; ++i) {
 				for (int j = mid; j < mat[0].length; ++j) {
 					result.append(mat[i][j].latex());
@@ -104,13 +105,14 @@ public class Matrix<T extends Arithmetic<T> & Parse & Latex & HasDelim> {
 				}
 			}
 		}
-		
+		*/
+		//result.append("\\begin{gmatrix}");
 		if(ops!="") {
-			result.append("\n\\rowops\n");
+			result.append("\\rowops\n");
 			result.append(ops);
 		}
 		
-		result.append("\n\\end{gmatrix}$\n\n");
+		result.append("\\end{gmatrix}\\\\\n");
 		return result.toString();
 	}
 	private int firstColEntryNeq0(int col,int start){
